0f973f2f060c801acf1d0cbcde60af615927d49b
Blitzkrieg69 <dhanishsoni1@gmail.com>
2025-01-22 22:34:45 +0530

    Updated

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,9 +5,12 @@ const Home = () => {
   return (
     <div className='home'>
       <div className="hero">
-        <h1>Largest <br/> Crypto Marketplace</h1>
-        <p></p>
-        Discover the latest trends in the cryptocurrency market. Stay updated with real-time data and make informed decisions to maximize your investments.
+        <h1>Largest <br /> Crypto Marketplace</h1>
+        <p>
+          Thy greens run long, thy red be none,   with crypto, as it is with options.
+          Give us Today our daily gains, and forgive us our losses, as we forgive those who short against us.
+          And not lead us into margin call, but deliver us our tredies now untill forever.Amen.
+        </p>
         <form>
           <input type="text" placeholder='Search Crypto' />
           <button type="submit">Search</button>
@@ -18,7 +21,7 @@ const Home = () => {
           <p>#</p>
           <p>Coin</p>
           <p>Price</p>
-          <p style={{textAlign:"center"}}>24 Hr</p>
+          <p style={{ textAlign: "center" }}>24 Hr</p>
           <p className='market-cap'>Market Cap</p>
         </div>
       </div>
